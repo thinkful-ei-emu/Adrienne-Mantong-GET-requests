05527dc0af75c0fe351d280d6339554e67251595
Mantong Zhao <mantong.cn@gmail.com>
2019-06-11 15:15:26 -0700

    dog request

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
 'use strict';
-
-// const imgUrl = 'https://dog.ceo/api/breeds/image/random';
-
-// function formatSearch(params) {
-//   const searchItems = Object.keys(params)
-//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-//   console.log(searchItems);
-//   return searchItems.join('');
-// }
+/* global $ */
 
 function getDogImage(num) {
-  let url = 'https://dog.ceo/api/breeds/image/random/' + num; 
-  console.log(url);
-  // const params = {
-  //   num, 
-  // };
-  // const searchString = formatSearch(params);
-  // const url = imgUrl + '/' + searchString;
-  fetch(url)
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+  let requestUrl = 'https://dog.ceo/api/breeds/image/random/' + num;
+  fetch(requestUrl)
+    .then(response => {
+      // console.log(response)
+      return response.json();
+    })
+    .then(responseJson => {
+      displayResults(responseJson);
+      // console.log(responseJson);
+    })
+    .catch(error => alert(`Something went wrong. Try again later. ${error.message}`));
 }
+
+
 
 function displayResults(responseJson) {
-  console.log(responseJson);
+  // console.log(responseJson);
+  //replace the existing image with the new ones
+  let images = '';
+  responseJson.message.forEach((image) => {
+    images += `<img src="${image}" class="results-img">`;
+  }); 
 
-  //replace the existing image with the new one
-  responseJson.message.forEach(img => {
-    $('.results').append(
-      `<img src="${img}" class="results-img">`
-    );
-    //display the results section
-    $('.results').removeClass('hidden');
-  });
+  $('.results-img').html(
+    images
+  );
+  //display the results section
+  $('.results').removeClass('hidden');
 }
 
-function watchForm() { 
-  $('#many-dogs').submit(event => { event.preventDefault(); 
-    let num = ($('#num').val() > 0 && $('#num').val() < 51) ? $('#num').val() : '3'; $('#num').val('');
-
-    getDogImage(num);
+function watchForm() {
+  $('#many-dogs').submit(event => {
+    event.preventDefault();
+    let dogNum = ($('#dog-number').val() > 0 && $('#dog-number').val() < 51) ? $('#dog-number').val() : '3';
+    $('#dog-number').val('');
+   
+    getDogImage(dogNum);
   });
 
 }
-
-// function randomForm() {
-//   $('.numOfDogs').submit(event => {
-//     event.preventDefault();
-//     displayResults(responseJson);
-//   }
-//     )
-// }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
