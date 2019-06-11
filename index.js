0247603ef6fa309244523ed3576f4d9abaa15a20
Adrienne Smith <agsmith046@gmail.com>
2019-06-11 11:35:52 -0500

    started work on random dog images

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function getDogImage() {
+  fetch('https://dog.ceo/api/breeds/image/random/50')
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+
+  //replace the existing image with the new one
+  responseJson.message.forEach(img => {
+    $('.results-img').replaceWith(
+      `<img src="${img}" class="results-img">`
+    );
+    //display the results section
+    $('.results').removeClass('hidden');
+  });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+// function randomForm() {
+//   $('.numOfDogs').submit(event => {
+//     event.preventDefault();
+//     displayResults(responseJson);
+//   }
+//     )
+// }
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
