f3bb08a194e60bd9eb9b8419678811475526813f
Adrienne Smith <agsmith046@gmail.com>
2019-06-11 20:43:09 -0500

    dog breed function working

--- dog-app-3/index.js
+++ dog-app-3/index.js
@@ -1,44 +1,38 @@
 'use strict';
-/* global $ */
 
-function getDogImage(breed) {
-  let requestUrl = 'https://dog.ceo/api/breed/hound/images';
+function getDogImage(breed, responseJson) {
+  let requestUrl = 
+  'https://dog.ceo/api/breed/hound-' + breed + '/images/random'
   fetch(requestUrl)
     .then(response => {
-      // console.log(response)
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(responseJson.status);
     })
-    .then(responseJson => {
-      displayResults(responseJson);
-      // console.log(responseJson);
-    })
-    .catch(error => alert(`Something went wrong. Try again later. ${error.message}`));
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+      alert($('#js-error-message').text(`Something went wrong: ${error.message}`));
+    });
 }
 
-
-
 function displayResults(responseJson) {
-  // console.log(responseJson);
-  //replace the existing image with the new ones
-  let images = '';
-  $('.results-img').replaceWith( `<img src="${responseJson.message}" class="results-img">` );
-  
-  $('.results-img').html(
-    images
-  );
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
   //display the results section
   $('.results').removeClass('hidden');
 }
 
-
 function watchForm() {
-  $('#many-dogs').submit(event => {
+  $('form').submit(event => {
     event.preventDefault();
-    
-   
-    getDogImage();
+    let dogBreed = ($('#breed').val());
+    $('#breed').val('');
+    getDogImage(dogBreed);
   });
-
 }
 
 $(function() {
